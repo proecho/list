66727df8b3f668f16121d655e29f58513caa5973
proecho <niamhgillespie2016@outlook.com>
2020-10-15 18:17:45 +0100

    major rework

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,35 @@
-use objects::entrys;
+//use objects::entrys;
 
-struct myList<T> {
-	head: Raw<T>,
-	tail: Raw<T>,
-	length: u64,
+//struct myList<T> {
+	//head: Raw<T>,
+	//tail: Raw<T>,
+	//length: u64,
 
-#[derive(Debug,PartialEq,Clone)]
-struct a_linked_list<T> {
-	before: Option(Raw<T>),
-	after: Option(Raw<T>),
-	content: Option<entrys<'a>>,
-}
+//#[derive(Debug,PartialEq,Clone)]
+//struct a_linked_list<T> {
+	//before: Option(Raw<T>),
+	//after: Option(Raw<T>),
+	//content: Option<entrys<'a>>,
+//}
 
-struct Raw<T>{
-	p: *mut T,
-}
+//struct Raw<T>{
+	//p: *mut T,
+//}
 
-impl myList<T> {
-	fn compose(&mut self,value:a_linked_list) {
-		if self.length == 0 {
-			self.replacetail(&value);
-			self.replacehead(&value);
+//impl myList<T> {
+	//fn compose(&mut self,value:a_linked_list) {
+	
+		//if self.length == 0 {
+			//self.replacetail(&value);
+			//self.replacehead(&value);
 		
-		}else{
-			self.tail.replace_before(&value);
-			self.replace_tail(&value);
-		}
+		//}else{
+			//self.tail.replace_before(&value);
+			//self.replace_tail(&value);
+		//}
 		
-		mylist.length += 1;
-	} 
+		//mylist.length += 1;
+	//} 
 		 
 
 
